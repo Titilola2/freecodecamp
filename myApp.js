d615df0d41dd7ea89685a10a851bb2b75094d22b
Titilola2 <oluwafunhmi@gmail.com>
2023-08-26 18:19:28 +0100

    helloworld

--- myApp.js
+++ myApp.js
@@ -5,6 +5,15 @@ let app = express();
 
 
 
+const bodyParser = require('body-parser');
+
+// Define a route handler for the root URL
+app.get('/', (req, res) => {
+    console.log("hello world"); // Log "hello world"
+    res.send("hello world"); // Send "hello world" as the response
+});
+
+
 
 
 
